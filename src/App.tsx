51e8ce4270ad23eae646443250f2ffc624c195c4
Sther <sthefanypequeno@gmail.com>
2023-08-15 14:29:46 -0300

    feat: creating first component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import  Subtitle  from './components/Subtitle'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <Subtitle />
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
---
+++ src/components/Subtitle.tsx
@@ -0,0 +1,9 @@
+const Subtitle = () => {
+    return(
+        <>
+        <h1>Hello World</h1>
+        </>
+    )
+}
+
+export default Subtitle;
